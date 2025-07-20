4745f3a86556b9d2ad4d867ee1c181035abf4e73
nihala shirin <nihalashirin02@gmail.com>
2025-07-21 01:23:37 +0530

    otp page

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path, include
-from .views import RegisterView
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import (
+    RegisterView, OTPVerification, CustomTokenObtainPairView, ResendOtpView, 
+    ForgetPassword, Logout, 
+)
+
+# Setting up the DefaultRouter for StudentProfile and LandingPage
 router = DefaultRouter()
 
-
 urlpatterns = [
-    path('user/register/',RegisterView.as_view(), name='user_register'),
+    path('', include(router.urls)),  # Includes the router URLs for the viewsets
+    path('register/', RegisterView.as_view(), name='register'),  # Registration route
+    path('otp-verify/', OTPVerification.as_view(), name='otp_verify'),  # OTP verification
+    path('forget-password/', ForgetPassword.as_view(), name='forget_password'),  # Forgot password
+    path('otp-resend/', ResendOtpView.as_view(), name='otp_resend'),  # Resend OTP
+    path('login/token/', CustomTokenObtainPairView.as_view(), name='login_token_obtain_pair'),  # JWT login route
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Token refresh route for JWT
+    path('logout/', Logout.as_view(), name='logout'),  # Logout route
 ]
+
--- account/views.py
+++ account/views.py
@@ -1,9 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
-from .serializers import UserSerializers  
 from rest_framework_simplejwt.tokens import RefreshToken  
 from .models import CustomUser
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .signal import generate_otp, send_otp_email
+
+from .api.user_serializers import (
+    UserSerializers, 
+    CustomTokenObtainPairSerializer, 
+    UserStatusSerializer, 
+    ChangePasswordSerializer
+)
 
 class RegisterView(APIView):
     """
@@ -26,9 +34,105 @@ class RegisterView(APIView):
         return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class OTPVerification(APIView):
+    """
+    Handles OTP verification for user registration or password reset.
+    """
+    def post(self, request):
+        email = request.data.get('email')
+        otp = request.data.get('otp')
+
+        # Check if the user exists
+        try:
+            user = CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND, data='User not found')
+        
+        # Check OTP expiration and validity
+        if not user.otp:
+            return Response(status=status.HTTP_400_BAD_REQUEST,data='OTP is Expired')
+
+        if user.otp == otp:
+            user.otp = None
+            user.is_verified = True
+            user.save()
+            return Response(data='OTP verifed successfully', status=status.HTTP_200_OK)
+        return Response(data='Invalid OTP', status=status.HTTP_400_BAD_REQUEST)
+        
+
+class ResendOtpView(APIView):
+    """
+    Resends OTP for verification.
+    """
+    def post(self,request):
+        email = request.data.get('email')
+
+        if not email:
+            return Response(data='Email is required', status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if user exists
+        try:
+            user = CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            return Response(data="User not Found", status=status.HTTP_404_NOT_FOUND)
+        
+       # Generate and send new OTP
+        otp = generate_otp()
+        user.otp = otp
+        user.save()
+        send_otp_email(email, otp)
+
+        return Response(data='OTP resent successfully', status=status.HTTP_200_OK)
+
+    
+class CustomTokenObtainPairView(TokenObtainPairView):
+    """
+    Custom Token Obtain Pair View using a custom serializer.
+    """
+    serializer_class = CustomTokenObtainPairSerializer
 
 
+class ForgetPassword(APIView):
+    """
+    Handles the password reset process including OTP generation.
+    """
+    def post(self, request):
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
 
+        if not email:
+            return Response(data={'error': 'Email is required'}, status=status.HTTP_400_BAD_REQUEST)
 
+        # Check if user exists
+        try:
+            user = CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            return Response(data={'error': 'User account not found'}, status=status.HTTP_404_NOT_FOUND)
 
+        # If password is provided, change it directly
+        if password:
+            user.set_password(password)
+            user.save()
+            return Response(data={'message': 'Password changed successfully'}, status=status.HTTP_201_CREATED)
 
+        # If password is not provided, generate OTP for password reset
+        otp = generate_otp()
+        user.otp = otp
+        user.save()
+        send_otp_email(email, otp)
+
+        return Response(data={'message': 'OTP sent for password reset', 'role': user.role}, status=status.HTTP_200_OK)
+       
+
+class Logout(APIView):
+    """
+    Handles user logout and token blacklist.
+    """
+    def post(self, request):
+        try:
+            refresh = request.data['refresh']
+            token = RefreshToken(refresh)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
