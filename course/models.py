9fd6365068aa1f1589701de59b83ccd97bb22fa3
nihala shirin <nihalashirin02@gmail.com>
2025-11-29 14:49:47 +0530

    completed

--- course/models.py
+++ course/models.py
@@ -73,7 +73,6 @@ class Video(models.Model):
     title = models.CharField(max_length=200)
     video_url = models.URLField()
     duration = models.CharField(max_length=50, blank=True, null=True)
-    order = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
